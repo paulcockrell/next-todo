1c8e95ec4d1f398352b2c2a74d77eabf53ca3a9a
Paul Cockrell <paul.cockrell@duradiamondsoftware.co.uk>
2021-03-12 06:55:22 +0000

    Protect create todo api

--- pages/api/createTodo.js
+++ pages/api/createTodo.js
@@ -1,10 +1,14 @@
 import { table } from "./utils/Airtable";
+import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
 
-export default async (req, res) => {
+export default withApiAuthRequired(async (req, res) => {
+  const { user } = getSession(req, res);
   const { description } = req.body;
 
   try {
-    const createdRecords = await table.create([{ fields: { description } }]);
+    const createdRecords = await table.create([
+      { fields: { description, userId: user.sub } },
+    ]);
     const createdRecord = {
       id: createdRecords[0].id,
       fields: createdRecords[0].fields,
@@ -17,4 +21,4 @@ export default async (req, res) => {
     res.statusCode = 500;
     res.json({ msg: "Something went wrong" });
   }
-};
+});
